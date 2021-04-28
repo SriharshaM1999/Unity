cb328fc5ea57a23f57e25110868b457b33a914de
SriharshaM1999 <sriharshamadireddy@gmail.com>
2021-04-28 23:08:41 +0530

    work on ejs layouts and css files

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const app= express();
 app.set("view engine","ejs");
 app.set("views","./views")
 app.use(express.static('./assets'))
+app.use(expressLayouts)
 
+app.set("layout ExtractStyles",true)
+app.set("layout ExtractScript",true)
 
 
 app.use('/', require("./routes"))
